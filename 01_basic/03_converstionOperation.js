1c5823069ebef4979a3adcbcdf9ccf63bf0eb562
singhrihan451-web <singhrihan451@gmail.com>
2025-09-22 16:59:42 +0000

    commit

--- 01_basic/03_converstionOperation.js
+++ 01_basic/03_converstionOperation.js
@@ -25,3 +25,41 @@ let someNumber =33
 let stringNumber =String(someNumber)
 console.log(stringNumber);                  //  33
 console.log( typeof stringNumber);          //  33  string
+
+
+
+
+//*******************Operation***************//
+let value =3
+let negValue =-value
+//console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1="hello"
+let str2=" vishal"
+let str3=str1+str2
+console.log(str3);   //hello vishal
+
+console.log("1" +2);       //12
+console.log(1 +"2");       //12
+console.log("1" +2+2);       //122
+console.log(1 +2 +"2");          //32
+
+console.log((3+4)*5% 3);
+
+console.log(true);    //true
+console.log(+true);      //1
+console.log(+"");       // 0
+
+let num1,num2,num3
+num1= num2 =num3=2+2
+
+let gamecounter =100
+gamecounter++;
+console.log(gamecounter);
