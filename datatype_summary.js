295e56f90bebc520a85c6b1d9696bbe640780671
singhrihan451-web <singhrihan451@gmail.com>
2025-09-23 17:30:25 +0000

    datatype summary

---
+++ datatype_summary.js
@@ -0,0 +1,32 @@
+// primitive datatype----(call by value)
+
+//7 types: String,number, Boolean, null, undefined, symbol, bigint
+
+const score =100
+const scorevalue=100.3
+
+const isloggedIn =false
+const outsideTemp =null
+console.log(typeof outsideTemp);      // ans--object
+let userEmail;
+
+const id= Symbol('123')
+const anotherId =Symbol('123')              //typeof---symbol
+console.log(id===anotherId);
+
+
+const bignumber =345628n
+
+//Reference (Non primitive)
+
+// Array, Objects , Functions
+ const heros =["shaktiman", "pookie"];
+ let myobj ={
+    name :"vishal",
+    age:22,
+ }
+
+ const myfunction =function(){
+    console.log("hello world");
+
+ }
