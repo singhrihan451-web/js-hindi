cf0cc314c36a7dadf020f7b653e514b701d021d3
vishal451 <singhrihan451@gmail.com>
2025-09-25 08:48:15 +0000

    commit

---
+++ 01_basic/05_string.js
@@ -0,0 +1,42 @@
+
+
+const name ="vishal"
+const repocount =50
+
+// console.log(name+""+ repocount+"" +"value");
+
+// console.log(`hello my name is ${name} and my repo count is ${repocount}`);
+
+const gamename =new String('vishal savita')
+console.log(gamename);
+
+// console.log(gamename[2]);
+// console.log(gamename.__proto__);
+// console.log(gamename.length);
+// console.log(gamename.toUpperCase());
+// console.log(gamename.toLowerCase());
+
+
+// console.log(gamename.charAt(2));
+// console.log(gamename.indexOf('s'));
+// const newString =gamename.substring(0,4)
+// console.log(newString);
+
+// const anotherString =gamename.slice(2,4)
+// console.log(anotherString);
+
+// const newstringone ="   vishal   "
+// console.log(newstringone);
+// console.log(newstringone.trim());
+
+
+// const url ="https://vishal@20.com"
+// console.log(url.replace('@20','-'));
+// console.log(url.includes('sundar'));
+
+console.log(gamename.split('-'));
+
+
+
+
+
