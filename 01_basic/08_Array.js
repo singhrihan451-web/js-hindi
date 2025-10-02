f60b38d476b44d88a9e0baeea4919ff09f5a0055
vishal451 <singhrihan451@gmail.com>
2025-10-02 16:05:15 +0000

    commit

---
+++ 01_basic/08_Array.js
@@ -0,0 +1,40 @@
+//array
+
+const myArr =[0,1,2,3,4,5]
+// const myHeros =["shaktiman","spiderman"]
+// const myArr2 =new Array(1,2,3,4)
+// console.log(myArr[0]);
+
+//ARRAY Method
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+
+// myArr.unshift(5)           //start me add ho jayega
+
+// myArr.shift()        //delete ho jayega
+
+// console.log(myArr.includes(9));        // ans - false
+// const newArr =myArr.join()
+// console.log(myArr);
+// console.log(newArr);                    // join--> array string me convert ho jayega ----0,1,2,3,4,5
+
+
+//slice ,splice
+console.log("A",myArr);
+const myn1 =myArr.slice(1,4)
+console.log(myn1);
+
+
+console.log("B",myArr);
+console.log(myn1);
+
+console.log("C",myArr);
+const myn2 =myArr.splice(1,5)
+console.log(myn2);
+
+
+
+
