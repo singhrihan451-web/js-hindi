0d8cf5c09c9c95e217961aa4cdabe3ebc804d4c7
vishal451 <singhrihan451@gmail.com>
2025-10-02 15:02:38 +0000

    commit

---
+++ 01_basic/07_datetime.js
@@ -0,0 +1,38 @@
+//date 
+let myDate= new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);                  //object
+
+
+// let myCreatedDate =new Date(2025,0,23)
+// let myCreatedDate =new Date(2025,0,23,5,3)              // date+time
+// let myCreatedDate =new Date("2025-01-02")
+let myCreatedDate =new Date("02-12-2025")
+// console.log(myCreatedDate.toDateString());
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp =Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate =new Date()
+// console.log(newDate);
+// console.log(newDate.getDay());
+// console.log(newDate.getMonth());
+// console.log(newDate.getMonth() + 1);
+
+
+// `${newDate.getDay()} and the time`
+
+let mydate = newDate.toLocaleString('default',{
+    weekday:"narrow"
+})
+console.log(mydate);
+
+
+
