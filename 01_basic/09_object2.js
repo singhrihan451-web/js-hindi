bb88a2452aa7424d48859836fb2b1db62cf69f41
vishal451 <singhrihan451@gmail.com>
2025-10-06 17:26:02 +0000

    commit

--- 01_basic/09_object2.js
+++ 01_basic/09_object2.js
@@ -21,5 +21,35 @@ const regularUser={
 const obj1={1:"a",2:"b"}
 const obj2={3:"a",4:"b"}
 
-const obj3 ={obj1,obj2}
-console.log(obj3);
+// const obj3 ={obj1,obj2}
+// const obj3= Object.assign({},obj1,obj2)
+
+
+const obj3 ={...obj1,...obj2}
+// console.log(obj3);
+
+
+const users=[
+    {
+        id:1,
+        email:"vk@gmail.com"
+    },
+    {
+        id:2,
+        email:"vk@gmail.com"
+    },
+    {
+        id:3,
+        email:"vk@gmail.com"
+    },
+]
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('issloggedin'));
+console.log(tinderUser.hasOwnProperty('islogged'));
+
