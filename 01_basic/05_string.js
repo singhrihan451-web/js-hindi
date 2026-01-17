f0cbf0a94032fce080e0600ec746931bbff073cd
vishal451 <singhrihan451@gmail.com>
2026-01-17 05:41:11 +0000

    newcommit

--- 01_basic/05_string.js
+++ 01_basic/05_string.js
@@ -7,7 +7,7 @@ const repocount =50
 
  //console.log(`hello my name is ${name} and my repo count is ${repocount}`);
 
-const gamename =new String('vishal savita')
+const gamename =new String('vishal-savita')    // new keyword used to reference of object to string
 console.log(gamename);
 
 // console.log(gamename[2]);
@@ -19,11 +19,11 @@ console.log(gamename);
 
 // console.log(gamename.charAt(2));
 // console.log(gamename.indexOf('s'));
-// const newString =gamename.substring(0,4)
-// console.log(newString);
+const newString =gamename.substring(2,4)       // print--vish  0-4 then 4 are not included same as well as slice method
+console.log(newString);                         // does'nt contain negative value start from the 0 index
 
-// const anotherString =gamename.slice(2,4)
-// console.log(anotherString);
+const anotherString =gamename.slice(2,4)    // cantain negative value
+console.log(anotherString);
 
 // const newstringone ="   vishal   "
 // console.log(newstringone);
@@ -32,9 +32,10 @@ console.log(gamename);
 
 // const url ="https://vishal@20.com"
 // console.log(url.replace('@20','-'));
-// console.log(url.includes('sundar'));
+// console.log(url.includes('sundar'));   // find this string is available in url that reply for true and false
 
-console.log(gamename.split('-'));
+console.log(gamename.split('-'));     // all string break into substring part in single string with (, - any symbol)
+console.log(gamename.split(','));     // all string break into substring part in single string with (, - any symbol)
 
 
 
