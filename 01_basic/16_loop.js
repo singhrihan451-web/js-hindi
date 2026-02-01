9015a0b0282318fc85c5ed342468d7ed7ba2cca2
vishal451 <singhrihan451@gmail.com>
2026-02-01 06:21:30 +0000

    commit

--- 01_basic/16_loop.js
+++ 01_basic/16_loop.js
@@ -9,14 +9,56 @@
 //     console.log(element);
 // }
 
-for (let i = 0; i <=10; i++) {
-    console.log(`outer loop value:${i}`);
+// let myArray =["flash","batman","superman"]
+// console.log(myArray.length);
+
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+    // console.log(element);
     
-    for (let j = 0; j <=10; j++) {
-        console.log(`inner loop value ${j} and inner loop ${i}`);
+
+// break and continue
+
+// for (let index = 1; index <=20; index++) {
+//     if(index==5){
+//         console.log("detected 5");
+//         break
         
-        
-    }
-  
-    
-}
+//     }
+//     console.log(`value od i is ${index}`);
+ 
+// }
+// for (let index = 1; index <=20; index++) {
+//     if(index==5){
+//         console.log("detected 5");
+//         continue  
+//     }
+//     console.log(`value od i is ${index}`);
+//  }
+
+// ****** While loop********
+
+// let index=2
+// while(index<=10){
+//     console.log(`value of index is ${index}`);
+//     index=index+2
+// }
+
+// let myArray=['flash','batman',"superman"]
+
+// let arr=0
+
+// while (arr < myArray.length) {
+//     console.log(`value is ${myArray[arr]}`);
+//     arr = arr + 1
+// }
+
+
+// ********** DO while***********
+
+let score =11
+
+do {
+    console.log(`score is${score}`);
+    score++
+} while (score <=10);
