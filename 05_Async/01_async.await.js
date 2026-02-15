35cef58baceff68feaaee37c627a2cf8fed7c562
vishal451 <singhrihan451@gmail.com>
2026-02-15 08:02:47 +0000

    commit

---
+++ 05_Async/01_async.await.js
@@ -0,0 +1,13 @@
+    ``
+
+JAVASCRIPT
+   -->Synchronous
+   -->Single threaded 
+                           default
+
+EXECUTION Context
+-->execute one line of code at a time 
+>>console log ->1                             each operation waits for the last
+>>console log ->2                           one to complete before execution
+
+CALL STACK                        MEMORY HEAP
